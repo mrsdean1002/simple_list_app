6338850503b672c6873c792ac30ff5bdca76879c
Tamika Dean <mrsdean1002@gmail.com>
2018-06-26 02:25:20 -0400

    add .gitignore,index.html,package-lock.json,package.json, and server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+// All handlers below...
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
+
+app.listen(3000, function(){
+  console.log('listening on port 3000')
+})
